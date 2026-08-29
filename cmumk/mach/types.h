2b833b895ce09d3fd6071a2e3907e73d6abc1875
1Abc342PedroGina-novo <rmagalhaesjuridico5@gmail.com>
2026-08-29 16:36:21 -0300

    Add _LIST_ENTRY structure definition

--- cmumk/mach/types.h
+++ cmumk/mach/types.h
@@ -56,4 +56,11 @@ typedef	natural_t	vm_size_t;
 #define VM_MIN_KERNEL_ADDRESS   ((ULONGLONG) 0xffff800000000000) // Início do espaço do Kernel
 #define VM_MAX_KERNEL_ADDRESS   ((ULONGLONG) 0xffffffffffffffff) // Topo absoluto da memória
 
+//0x10 bytes (sizeof)
+struct _LIST_ENTRY
+{
+    struct _LIST_ENTRY* Flink;                                              //0x0
+    struct _LIST_ENTRY* Blink;                                              //0x8
+}; 
+
 #endif
